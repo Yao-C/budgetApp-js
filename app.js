3757e76dfd43efdfe819040e6d94cf18637a9256
YAO CHENG <yao.cheng@norskluftambulanse.no>
2019-02-26 11:16:45 +0100

    4: UICtrl-return-clearFields, slice-list-to-array, controller-ctrlAddItem-clearFields

--- app.js
+++ app.js
@@ -137,7 +137,19 @@ var UIController =(function() {
 
       // Insert HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
-    }
+    },
+
+    clearFields: function() {
+      var fields, fieldsArr;
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+      // fields returned is not a array, is a nodeList, so can not use fields.forEach(), need to use slice.call return an array first
+      fieldsArr = Array.prototype.slice.call(fields);
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+      // After clearing input fields, set focus back to description
+      fieldsArr[0].focus();
+    },
   };
 })(); 
 
@@ -180,6 +192,9 @@ var controller =(function(budgetCtrl, UICtrl) {
     var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
+    // 4. Clear the fields
+    UICtrl.clearFields();
+
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
